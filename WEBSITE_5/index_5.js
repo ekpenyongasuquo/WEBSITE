f2b571e71509ac61b3ce6398a15de3cce67a4ed9
ekpenyongasuquo <eamfon@outlook.com>
2023-12-12 14:21:32 +0100

    Add indexes

--- WEBSITE_5/index_5.js
+++ WEBSITE_5/index_5.js
@@ -1,2 +1,14 @@
 const myButton = document.querySelector("#myButton");
-myButton = addEventListener("click", () => window.open("http://google.com"));
+
+
+//myButton = addEventListener("click", () => window.open("http://google.com"));
+
+//myButton = addEventListener("click", () => window.close());
+
+//myButton = addEventListener("click", () => window.print());
+
+let age = window.prompt("Enter your age:");
+if (age < 18) {
+    window.alert("You must be 18 and above to visit this site");
+    window.close();
+}
---
+++ WEBSITE_5/index_6.js
@@ -0,0 +1,3 @@
+//cookies: a small text file stored on your computer used to remeber information about the user saved in name = value pairs
+
+console.log(navigator.cookieEnabled);
